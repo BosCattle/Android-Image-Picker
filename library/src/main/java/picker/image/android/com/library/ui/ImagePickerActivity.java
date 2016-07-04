58422f5d7273b82fb0a6cb77e8e37dcb5ce6f91b
Kevin <jiangtao103cp@163.com>
2016-07-04 22:57:36 +0800

    fix

--- library/src/main/java/picker/image/android/com/library/ui/ImagePickerActivity.java
+++ library/src/main/java/picker/image/android/com/library/ui/ImagePickerActivity.java
@@ -1,22 +1,25 @@
 package picker.image.android.com.library.ui;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
+ import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import picker.image.android.com.library.R;
 import picker.image.android.com.library.option.ImageOption;
 
-public class ImagePickerActivity extends AppCompatActivity {
+/**
+ * picker image by kevin
+ */
+public class ImagePickerActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
   public static final int CONSTANT_REQUEST_CODE = 100;
 
   private Toolbar mToolbar;
   private RecyclerView mRecyclerView;
-  private FloatingActionButton mFloatingActionButton;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -26,7 +29,6 @@ public class ImagePickerActivity extends AppCompatActivity {
 
   private void initialization() {
     mToolbar = (Toolbar) findViewById(R.id.toolbar);
-    mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
     mRecyclerView = (RecyclerView) findViewById(R.id.ui_image_recycler);
     mToolbar.setTitle(getResources().getString(R.string.label_toolbar));
     mToolbar.setNavigationIcon(android.R.drawable.ic_menu_crop);
@@ -34,14 +36,21 @@ public class ImagePickerActivity extends AppCompatActivity {
 
   /**
    * open image picker activity
-   * @param activity
-   * @param option
-   * @param code
    */
   public static void openActivity(AppCompatActivity activity, ImageOption option, int code) {
     Intent intent = new Intent(activity, ImagePickerActivity.class);
     activity.startActivityForResult(intent, code);
   }
 
+  @Override public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    return null;
+  }
 
+  @Override public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+  }
+
+  @Override public void onLoaderReset(Loader<Cursor> loader) {
+
+  }
 }
